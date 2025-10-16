7080b0d643e3e49addca8ecbdac8b68ebc2aa8e0
semaybulut@gmail.com <semaybulut@gmail.com>
2025-10-16 17:49:27 +0000

    testler eklendi

--- models/stg_raw__product.sql
+++ models/stg_raw__product.sql
@@ -1,15 +1,13 @@
-select * from renamed
-
 with
- source as (
-  select * from {{ source('raw', 'product') }}
- ),
- renamed as (
-  select
-   products_id,
-   cast(purchSE_PRICE as float64) as purchase_price
-  from source
- )
-select * from renamed
+    source as (select * from {{ source("raw", "product") }}),
+    renamed as (
+        select 
+        products_id, 
+        cast(purchse_price as float64) as purchase_price 
+        from source
+    )
+select *
+from renamed
 
---dbt run -m stg_raw__product.sql >> burda -m demek -- demek
+    -- dbt run -m stg_raw__product.sql >> burda -m demek -- demek
+    
